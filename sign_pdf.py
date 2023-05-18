ecd729b4a2a09f8756eb6669dce2f60fdb50d29f
zakiazhari <78516830+zakiazhari@users.noreply.github.com>
2023-05-18 14:40:04 +0700

    self signed

--- sign_pdf.py
+++ sign_pdf.py
@@ -11,3 +11,14 @@ def createKeyPair(type, bits):
     pkey = OpenSSL.crypto.PKey()
     pkey.generate_key(type, bits)
     return pkey
+# Create a self signed certificate
+def create_self_signed_cert(pKey):
+    cert = OpenSSL.crypto.X509()
+    cert.get_subject().CN = "NAUFAL ALIF"
+    cert.set_serial_number(int(time.time() * 10))
+    cert.gmtime_adj_notBefore(0)
+    cert.gmtime_adj_notAfter(10 * 365 * 24 * 60 * 60)
+    cert.set_issuer((cert.get_subject()))
+    cert.set_pubkey(pKey)
+    cert.sign(pKey, 'md5')
+    return cert
